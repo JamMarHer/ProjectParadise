fe9525c1f3af9cac605cad2073cdb5566d09f92f
Jam <jamarck96@gmail.com>
2018-04-13 15:18:56 -0400

    added more error codes and explanations

--- app/src/main/java/paradise/ccclxix/projectparadise/BackendVals/ErrorCodes.java
+++ app/src/main/java/paradise/ccclxix/projectparadise/BackendVals/ErrorCodes.java
@@ -2,10 +2,18 @@ package paradise.ccclxix.projectparadise.BackendVals;
 
 public class ErrorCodes {
 
+    // Intended code ranges; 101-200 Network
+    //                       200-250 Registration
+    //                       250-300 Login
+    //                       300-*   DB Exceptions and more.
+    // Registration related.
     public final static int USER_NOT_AVAILABLE = 200;
     public final static int USERNAME_DOES_NOT_EXIST = 201;
     public final static int ID_DOES_NOT_EXIST = 202;
     public final static int EMAIL_NOT_AVAILABLE = 202;
+
+    // Login related
+    public final static int INCORRECT_PASSWORD = 250;
 
     public final static int PDO_EXCEPTION = 300; //General
 

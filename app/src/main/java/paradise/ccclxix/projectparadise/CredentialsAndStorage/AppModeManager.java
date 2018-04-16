2bf0c69fce61cb20303c51692e6eff04fc9c647b
Jam <jamarck96@gmail.com>
2018-04-16 19:52:32 -0400

    Added appmodeManager, it keeps track how the mainActivity should run.

---
+++ app/src/main/java/paradise/ccclxix/projectparadise/CredentialsAndStorage/AppModeManager.java
@@ -0,0 +1,40 @@
+package paradise.ccclxix.projectparadise.CredentialsAndStorage;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import static android.content.Context.MODE_PRIVATE;
+
+public class AppModeManager {
+
+    private Context context;
+    private SharedPreferences sharedPreferences;
+    private final String APP_MODE = "iDaeAppMode";
+
+
+    public AppModeManager(Context context){
+        this.context = context;
+        this.sharedPreferences = this.context.getSharedPreferences(APP_MODE, MODE_PRIVATE);
+
+    }
+
+    public String getMode(){ return sharedPreferences.getString("mode","explore"); }
+
+    public void  setModeToExplore(){
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("mode","explore");
+        editor.apply();
+    }
+
+    public void  setModeToHost(){
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("mode","host");
+        editor.apply();
+    }
+
+    public void  setModeToAttendant(){
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("mode","attendant");
+        editor.apply();
+    }
+}

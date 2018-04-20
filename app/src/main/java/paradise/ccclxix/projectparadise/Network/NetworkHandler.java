af03d2ae8e4b60fc7664f5da4d392d4b6ffd3784
Jam <jamarck96@gmail.com>
2018-04-20 17:04:29 -0400

    Added login user tp network manager

--- app/src/main/java/paradise/ccclxix/projectparadise/Network/NetworkHandler.java
+++ app/src/main/java/paradise/ccclxix/projectparadise/Network/NetworkHandler.java
@@ -11,6 +11,7 @@ import paradise.ccclxix.projectparadise.APIForms.UserResponse;
 import paradise.ccclxix.projectparadise.APIServices.iDaeClient;
 import paradise.ccclxix.projectparadise.BackendVals.ConnectionUtils;
 import paradise.ccclxix.projectparadise.BackendVals.ErrorCodes;
+import paradise.ccclxix.projectparadise.Login.LoginActivity;
 import paradise.ccclxix.projectparadise.MainActivity;
 import paradise.ccclxix.projectparadise.R;
 import paradise.ccclxix.projectparadise.Registration.RegistrationActivity;
@@ -24,6 +25,40 @@ public class NetworkHandler {
 
     private NetworkResponse networkResponse;
     private boolean running = false;
+
+    public void loginNetworkRequest(final User user){
+        running = true;
+        Retrofit.Builder builder = new Retrofit.Builder()
+                .baseUrl(ConnectionUtils.MAIN_SERVER_API)
+                .addConverterFactory(GsonConverterFactory.create());
+        Retrofit retrofit = builder.build();
+
+        iDaeClient iDaeClient = retrofit.create(paradise.ccclxix.projectparadise.APIServices.iDaeClient.class);
+
+        Call<UserResponse> call = iDaeClient.loginUser(user);
+
+        call.enqueue(new Callback<UserResponse>() {
+            @Override
+            public void onResponse(Call<UserResponse> call, Response<UserResponse> response) {
+                System.out.println(response.raw());
+                if (response.body().getStatus() == ErrorCodes.INCORRECT_LOGIN) {
+                    networkResponse = new NetworkResponse(ErrorCodes.INCORRECT_LOGIN, response.body());
+                } else if (response.body().getStatus() == 100) {
+                    networkResponse = new NetworkResponse(100, response.body());
+                } else {
+                    networkResponse = new NetworkResponse(ErrorCodes.INCORRECT_FORMAT, response.body());
+                }
+                running = false;
+
+            }
+
+            @Override
+            public void onFailure(Call<UserResponse> call, Throwable t) {
+                networkResponse = new NetworkResponse(ErrorCodes.FAILED_CONNECTION, null);
+                running = false;
+            }
+        });
+    }
 
 
     public void addUserNetworkRequest(final User user){

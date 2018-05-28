fbc6a24121797109d58c5e6fa13bb941ec7539c0
Jam <jamarck96@gmail.com>
2018-05-27 21:20:58 -0400

    Added stub Activity to hold new post information.

---
+++ app/src/main/java/paradise/ccclxix/projectparadise/Fragments/WaveRelated/WaveAddPostActivity.java
@@ -0,0 +1,15 @@
+package paradise.ccclxix.projectparadise.Fragments.WaveRelated;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import paradise.ccclxix.projectparadise.R;
+
+public class WaveAddPostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wave_add_post);
+    }
+}

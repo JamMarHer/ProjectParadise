1eab73637c7b6163fd0c93833e1a58e0e21e9631
Jam <jamarck96@gmail.com>
2018-05-27 22:14:01 -0400

    Defined some of the layout properties in the waveaddpost activity.

--- app/src/main/java/paradise/ccclxix/projectparadise/Fragments/WaveRelated/WaveAddPostActivity.java
+++ app/src/main/java/paradise/ccclxix/projectparadise/Fragments/WaveRelated/WaveAddPostActivity.java
@@ -2,14 +2,34 @@ package paradise.ccclxix.projectparadise.Fragments.WaveRelated;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import paradise.ccclxix.projectparadise.R;
 
 public class WaveAddPostActivity extends AppCompatActivity {
 
+    private TextView waveAddPostUsername;
+    private TextView waveAddPostWave;
+    private EditText waveAddPostMessage;
+    private ImageView waveAddPostThumbnail;
+    private ImageView waveAddPostInsertImage;
+    private ImageView waveAddPostImage;
+    private ImageView waveAddPostCreatePost;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wave_add_post);
+
+        waveAddPostUsername = findViewById(R.id.wave_add_post_username);
+        waveAddPostWave = findViewById(R.id.wave_add_post_wave);
+        waveAddPostMessage = findViewById(R.id.wave_add_post_message);
+        waveAddPostThumbnail = findViewById(R.id.wave_add_post_thumbnail);
+        waveAddPostInsertImage = findViewById(R.id.wave_add_post_insert_image);
+        waveAddPostImage = findViewById(R.id.wave_add_post_image);
+        waveAddPostCreatePost = findViewById(R.id.wave_add_post_send);
     }
 }

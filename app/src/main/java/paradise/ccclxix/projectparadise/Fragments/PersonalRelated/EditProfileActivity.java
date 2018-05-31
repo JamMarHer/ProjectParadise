e773c4f7f0f979908ed62be36c7e6981b91a51ad
Jam <jamarck96@gmail.com>
2018-05-31 17:09:20 -0400

    Added stub for edit profile activity.

---
+++ app/src/main/java/paradise/ccclxix/projectparadise/Fragments/PersonalRelated/EditProfileActivity.java
@@ -0,0 +1,15 @@
+package paradise.ccclxix.projectparadise.Fragments.PersonalRelated;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import paradise.ccclxix.projectparadise.R;
+
+public class EditProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+    }
+}

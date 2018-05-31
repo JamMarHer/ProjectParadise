a13a1336e0b6b8a91fc7b68fb9470d2ad8566b45
Jam <jamarck96@gmail.com>
2018-05-31 14:55:08 -0400

    Made a class to hold icon values.

---
+++ app/src/main/java/paradise/ccclxix/projectparadise/utils/Icons.java
@@ -0,0 +1,10 @@
+package paradise.ccclxix.projectparadise.utils;
+
+import paradise.ccclxix.projectparadise.R;
+
+public class Icons {
+
+    public static final int FIRE = R.drawable.fire_emoji;
+    public static final int POOP = R.drawable.poop_icon;
+    public static final int COOL = R.drawable.cool;
+}

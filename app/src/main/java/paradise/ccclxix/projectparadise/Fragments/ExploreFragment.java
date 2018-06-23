a20fbfc0b6ff62fcb2b0495622377459a50c936f
Jam <jamarck96@gmail.com>
2018-06-23 16:04:57 -0400

    removed my waves from explore. Code is still present.

--- app/src/main/java/paradise/ccclxix/projectparadise/Fragments/ExploreFragment.java
+++ app/src/main/java/paradise/ccclxix/projectparadise/Fragments/ExploreFragment.java
@@ -16,9 +16,12 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -26,12 +29,23 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.OkHttp3Downloader;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
+import okhttp3.OkHttpClient;
 import paradise.ccclxix.projectparadise.Attending.QRScannerActivity;
 import paradise.ccclxix.projectparadise.CredentialsAndStorage.AppManager;
 import paradise.ccclxix.projectparadise.CredentialsAndStorage.ModeManager;
@@ -42,6 +56,7 @@ import paradise.ccclxix.projectparadise.Fragments.ChatFragmentRelated.EventChat;
 import paradise.ccclxix.projectparadise.Fragments.ChatFragmentRelated.OnGoingChats;
 import paradise.ccclxix.projectparadise.Fragments.ExploreRelated.DiscoverFragment;
 import paradise.ccclxix.projectparadise.Fragments.ExploreRelated.MyWavesFragment;
+import paradise.ccclxix.projectparadise.Fragments.ExploreRelated.WaveOverviewActivity;
 import paradise.ccclxix.projectparadise.HolderFragment;
 import paradise.ccclxix.projectparadise.Hosting.CreateEventActivity;
 import paradise.ccclxix.projectparadise.MainActivity;
@@ -49,6 +64,7 @@ import paradise.ccclxix.projectparadise.R;
 import paradise.ccclxix.projectparadise.utils.FirebaseBuilder;
 import paradise.ccclxix.projectparadise.utils.Icons;
 import paradise.ccclxix.projectparadise.utils.SnackBar;
+import paradise.ccclxix.projectparadise.utils.UINotificationHelpers;
 
 public class ExploreFragment extends HolderFragment implements EnhancedFragment {
 
@@ -56,10 +72,22 @@ public class ExploreFragment extends HolderFragment implements EnhancedFragment 
     private ViewGroup container;
     private FirebaseBuilder firebase = new FirebaseBuilder();
     SnackBar snackbar = new SnackBar();
-    private Button joinWave;
-    private Button createWave;
 
     AppManager appManager;
+    EditText searchText;
+    RecyclerView results;
+
+
+    ArrayList<String> id;
+    ArrayList<String> name;
+    ArrayList<String> thumbnail;
+    ArrayList<String> type;
+
+    int MAX_SEARCH = 15;
+    Picasso picasso;
+    SearchAdapter searchAdapter;
+    ProgressBar progressBar;
+
 
     private List<HashMap<String, String>> waveList;
 
@@ -78,18 +106,12 @@ public class ExploreFragment extends HolderFragment implements EnhancedFragment 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View inflater1 = inflater.inflate(R.layout.fragment_explore_waves, null);
-        ViewPager viewPager =  inflater1.findViewById(R.id.explore_viewpager);
+        View inflater1 = inflater.inflate(R.layout.fragment_discover, null);
 
-        FragmentAdapter adapter = new FragmentAdapter(getChildFragmentManager());
-        adapter.addFragment(new DiscoverFragment(), "Discover");
-        adapter.addFragment(new MyWavesFragment(), "My waves");
-        viewPager.setAdapter(adapter);
-        TabLayout tabs = inflater1.findViewById(R.id.result_tabs);
-        tabs.setupWithViewPager(viewPager);
 
-        createWave = inflater1.findViewById(R.id.createWave);
-        joinWave = inflater1.findViewById(R.id.joinWave);
+
+        Button createWave = inflater1.findViewById(R.id.createWave);
+        Button joinWave = inflater1.findViewById(R.id.joinWave);
 
 
         createWave.setOnClickListener(new View.OnClickListener() {
@@ -107,36 +129,159 @@ public class ExploreFragment extends HolderFragment implements EnhancedFragment 
                 startActivity(intent);
             }
         });
-        return inflater1;
 
+        firebase = new FirebaseBuilder();
+        searchText = inflater1.findViewById(R.id.search_edit);
+        results = inflater1.findViewById(R.id.results_recycler_view);
+        progressBar = inflater1.findViewById(R.id.search_progress);
+        results.setHasFixedSize(true);
+        results.setLayoutManager(new LinearLayoutManager(getContext()));
+        results.addItemDecoration(new DividerItemDecoration(getContext(), LinearLayoutManager.VERTICAL));
+
+
+        OkHttpClient okHttpClient =  new OkHttpClient.Builder()
+                .build();
+
+        picasso = new Picasso.Builder(getActivity()).downloader(new OkHttp3Downloader(okHttpClient)).build();
+
+        id = new ArrayList<>();
+        name = new ArrayList<>();
+        thumbnail = new ArrayList<>();
+        type = new ArrayList<>();
+        searchText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (!editable.toString().isEmpty()){
+                    setAdapter(editable.toString());
+                }else {
+                    name.clear();
+                    thumbnail.clear();
+                    id.clear();
+                    type.clear();
+                    results.removeAllViews();
+
+                }
+            }
+        });
+
+        return inflater1;
     }
 
-    static class FragmentAdapter extends FragmentPagerAdapter {
-        private final List<Fragment> mFragmentList = new ArrayList<>();
-        private final List<String> mFragmentTitleList = new ArrayList<>();
 
-        public FragmentAdapter(FragmentManager manager) {
-            super(manager);
+    private void setAdapter(final String s){
+
+
+        UINotificationHelpers.showProgress(true,results, progressBar, getResources().getInteger(android.R.integer.config_shortAnimTime));
+        firebase.getEvents().addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                name.clear();
+                thumbnail.clear();
+                id.clear();
+                type.clear();
+                results.removeAllViews();
+                if (dataSnapshot.hasChildren()){
+                    int currentCount = 0;
+                    for (DataSnapshot waves : dataSnapshot.getChildren()){
+
+                        String currentId = waves.getKey();
+                        if (waves.child("privacy").getValue().toString().equals("false")){
+                            String currentUsername = waves.child("name_event").getValue().toString();
+                            String currentThumbnail = "";
+                            if (waves.hasChild("thumbnail")){
+                                currentThumbnail = waves.child("thumbnail").getValue().toString();
+                            }
+                            if (currentUsername.toLowerCase().contains(s.toLowerCase())){
+                                id.add(currentId);
+                                name.add(currentUsername);
+                                thumbnail.add(currentThumbnail);
+                                type.add("WAVE");
+                                currentCount++;
+                            }
+                            if (currentCount == MAX_SEARCH)
+                                break;
+                        }
+                    }
+                    UINotificationHelpers.showProgress(false,results, progressBar, getResources().getInteger(android.R.integer.config_shortAnimTime));
+
+                }
+
+                searchAdapter = new SearchAdapter(getContext());
+                results.setAdapter(searchAdapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    public class SearchAdapter extends RecyclerView.Adapter<SearchViewHolder>{
+        Context context;
+
+        public SearchAdapter(Context context){
+            this.context = context;
         }
 
         @Override
-        public Fragment getItem(int position) {
-            return mFragmentList.get(position);
+        public SearchViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(context).inflate(R.layout.single_wave_user_view, parent, false);
+            return new SearchViewHolder(view);
         }
 
         @Override
-        public int getCount() {
-            return mFragmentList.size();
-        }
+        public void onBindViewHolder(SearchViewHolder holder, final int position) {
+            holder.name.setText(name.get(position));
+            if (!thumbnail.get(position).isEmpty()){
+                picasso.load(thumbnail.get(position))
+                        .fit()
+                        .centerInside()
+                        .placeholder(R.drawable.ic_import_export).into(holder.thumbnail);
+            }
+            holder.view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent =  new Intent(getActivity(), WaveOverviewActivity.class);
+                    Bundle bundle = new Bundle();
+                    bundle.putString("ID", id.get(position));
+                    bundle.putString("thumbnail", thumbnail.get(position));
+                    bundle.putString("name", name.get(position));
+                    intent.putExtras(bundle);
+                    getActivity().startActivity(intent);
 
-        public void addFragment(Fragment fragment, String title) {
-            mFragmentList.add(fragment);
-            mFragmentTitleList.add(title);
+                }
+            });
         }
 
         @Override
-        public CharSequence getPageTitle(int position) {
-            return mFragmentTitleList.get(position);
+        public int getItemCount() {
+            return name.size();
+        }
+    }
+
+
+    public class SearchViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView thumbnail;
+        TextView name;
+        ConstraintLayout view;
+
+        public SearchViewHolder(View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.card_name);
+            thumbnail = itemView.findViewById(R.id.card_thumbnail);
+            view = itemView.findViewById(R.id.card_single_view);
         }
     }
 

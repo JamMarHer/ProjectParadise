93802dd47b889bf43fe1bae6dae162614247e2eb
Jam Hernandez <jamarck96@gmail.com>
2018-06-29 20:32:15 -0400

    Uhhh we lit. The first version of discover is working.

--- app/src/main/java/paradise/ccclxix/projectparadise/Fragments/ExploreFragment.java
+++ app/src/main/java/paradise/ccclxix/projectparadise/Fragments/ExploreFragment.java
@@ -3,13 +3,16 @@ package paradise.ccclxix.projectparadise.Fragments;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,13 +24,18 @@ import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.OkHttp3Downloader;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Random;
 
 import okhttp3.OkHttpClient;
 import paradise.ccclxix.projectparadise.Attending.QRScannerActivity;
@@ -49,6 +57,7 @@ public class ExploreFragment extends HolderFragment implements EnhancedFragment 
     private FirebaseBuilder firebase = new FirebaseBuilder();
     SnackBar snackbar = new SnackBar();
 
+    Random random = new Random();
     AppManager appManager;
     EditText searchText;
     RecyclerView results;
@@ -58,10 +67,14 @@ public class ExploreFragment extends HolderFragment implements EnhancedFragment 
     ArrayList<String> name;
     ArrayList<String> thumbnail;
     ArrayList<String> type;
+    ArrayList<String> numPosts;
+    ArrayList<String> numMembers;
+    ArrayList<String> wScore;
 
     int MAX_SEARCH = 15;
     Picasso picasso;
     SearchAdapter searchAdapter;
+    SuggestionsAdapter suggestionsAdapter;
     ProgressBar progressBar;
 
 
@@ -120,10 +133,15 @@ public class ExploreFragment extends HolderFragment implements EnhancedFragment 
 
         picasso = new Picasso.Builder(getActivity()).downloader(new OkHttp3Downloader(okHttpClient)).build();
 
+        // TODO maps?
         id = new ArrayList<>();
         name = new ArrayList<>();
+        numMembers = new ArrayList<>();
+        numPosts = new ArrayList<>();
+        wScore = new ArrayList<>();
         thumbnail = new ArrayList<>();
         type = new ArrayList<>();
+        setSuggestionsAdapter();
         searchText.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
@@ -138,13 +156,14 @@ public class ExploreFragment extends HolderFragment implements EnhancedFragment 
             @Override
             public void afterTextChanged(Editable editable) {
                 if (!editable.toString().isEmpty()){
-                    setAdapter(editable.toString());
+                    setSearchAdapter(editable.toString());
                 }else {
                     name.clear();
                     thumbnail.clear();
                     id.clear();
                     type.clear();
                     results.removeAllViews();
+                    setSuggestionsAdapter();
 
                 }
             }
@@ -154,7 +173,81 @@ public class ExploreFragment extends HolderFragment implements EnhancedFragment 
     }
 
 
-    private void setAdapter(final String s){
+    private void setSuggestionsAdapter(){
+        UINotificationHelpers.showProgress(true,results, progressBar, getResources().getInteger(android.R.integer.config_shortAnimTime));
+        firebase.getEvents().addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                name.clear();
+                thumbnail.clear();
+                id.clear();
+                type.clear();
+                numMembers.clear();
+                numPosts.clear();
+                wScore.clear();
+                results.removeAllViews();
+                if (dataSnapshot.hasChildren()){
+                    int currentCount = 0;
+                    long numChildren = dataSnapshot.getChildrenCount();
+                    for (DataSnapshot waves : dataSnapshot.getChildren()){
+
+                        String currentId = waves.getKey();
+                        if (waves.child("privacy").getValue().toString().equals("false")){
+                            String currentUsername = waves.child("name_event").getValue().toString();
+                            String currentThumbnail = "";
+                            if (waves.hasChild("thumbnail")){
+                                currentThumbnail = waves.child("thumbnail").getValue().toString();
+                            }
+                            if (getRandomTrue(currentCount, numChildren)){
+                                id.add(currentId);
+                                name.add(currentUsername);
+                                thumbnail.add(currentThumbnail);
+                                if (waves.child("wall").hasChild("posts"))
+                                    numPosts.add(String.valueOf(waves.child("wall").child("posts").getChildrenCount()));
+                                else
+                                    numPosts.add("0");
+                                if (waves.child("attending").hasChild("in"))
+                                    numMembers.add(String.valueOf(waves.child("attending").child("in").getChildrenCount()));
+                                else
+                                    numMembers.add("0");
+                                if (waves.hasChild("wave_score"))
+                                    wScore.add(waves.child("wave_score").getValue().toString());
+                                else
+                                    wScore.add("?");
+
+                                type.add("WAVE");
+                                currentCount++;
+                            }
+                            if (currentCount == MAX_SEARCH)
+                                break;
+                        }
+                    }
+                    UINotificationHelpers.showProgress(false,results, progressBar, getResources().getInteger(android.R.integer.config_shortAnimTime));
+
+                }
+
+                suggestionsAdapter = new SuggestionsAdapter(getContext());
+                results.setAdapter(suggestionsAdapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    // This is a bit interesting if you think of it. The method does provide randomness but it also
+    // treats with some degree of relevancy the newer waves.
+    private boolean getRandomTrue(int currentCount, long dataSnapshotCCount){
+        if (MAX_SEARCH - currentCount <= (int) dataSnapshotCCount){
+            return random.nextBoolean();
+        }else {
+            return true;
+        }
+    }
+
+    private void setSearchAdapter(final String s){
 
 
         UINotificationHelpers.showProgress(true,results, progressBar, getResources().getInteger(android.R.integer.config_shortAnimTime));
@@ -201,6 +294,49 @@ public class ExploreFragment extends HolderFragment implements EnhancedFragment 
 
             }
         });
+    }
+
+    public class SuggestionsAdapter extends RecyclerView.Adapter<SuggestionViewHolder>{
+
+        Context context;
+        public SuggestionsAdapter(Context context){
+            this.context = context;
+        }
+
+
+
+        @NonNull
+        @Override
+        public SuggestionViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(context).inflate(R.layout.wave_card_single, parent, false);
+            return new SuggestionViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull SuggestionViewHolder holder, final int position) {
+            holder.waveName.setText(name.get(position));
+
+            holder.mainLayout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent =  new Intent(getActivity(), WaveOverviewActivity.class);
+                    Bundle bundle = new Bundle();
+                    bundle.putString("ID", id.get(position));
+                    bundle.putString("thumbnail", thumbnail.get(position));
+                    bundle.putString("name", name.get(position));
+                    intent.putExtras(bundle);
+                    getActivity().startActivity(intent);
+                }
+            });
+            holder.wScore.setText(wScore.get(position));
+            holder.numMembers.setText(numMembers.get(position));
+            holder.numPosts.setText(numPosts.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return name.size();
+        }
     }
 
     public class SearchAdapter extends RecyclerView.Adapter<SearchViewHolder>{
@@ -258,6 +394,24 @@ public class ExploreFragment extends HolderFragment implements EnhancedFragment 
             name = itemView.findViewById(R.id.card_name);
             thumbnail = itemView.findViewById(R.id.card_thumbnail);
             view = itemView.findViewById(R.id.card_single_view);
+        }
+    }
+
+    public class SuggestionViewHolder extends RecyclerView.ViewHolder{
+
+        ConstraintLayout mainLayout;
+        TextView waveName;
+        TextView numPosts;
+        TextView numMembers;
+        TextView wScore;
+
+        public SuggestionViewHolder(View itemView){
+            super(itemView);
+            mainLayout = itemView.findViewById(R.id.wave_card_layout);
+            waveName = itemView.findViewById(R.id.wave_card_name);
+            numPosts = itemView.findViewById(R.id.wave_card_posts);
+            numMembers = itemView.findViewById(R.id.wave_card_members);
+            wScore = itemView.findViewById(R.id.wave_card_wScore);
         }
     }
 

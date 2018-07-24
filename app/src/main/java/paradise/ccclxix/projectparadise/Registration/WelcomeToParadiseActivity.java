4479a392856a1e79d85de4ab6aa7cd95f214dbe9
Jam Hernandez <jamarck96@gmail.com>
2018-07-24 16:26:30 -0400

    Removed the toolbar from the activity, also added a sample paradise icon.

--- app/src/main/java/paradise/ccclxix/projectparadise/Registration/WelcomeToParadiseActivity.java
+++ app/src/main/java/paradise/ccclxix/projectparadise/Registration/WelcomeToParadiseActivity.java
@@ -45,8 +45,6 @@ public class WelcomeToParadiseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_to_paradise);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());

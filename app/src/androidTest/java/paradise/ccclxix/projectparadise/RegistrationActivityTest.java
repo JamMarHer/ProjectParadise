9a8763adc882ec3485df808060baf554f55cf411
Jam Hernandez <jamarck96@gmail.com>
2019-02-28 12:37:27 -0500

    Lets fix this.

--- app/src/androidTest/java/paradise/ccclxix/projectparadise/RegistrationActivityTest.java
+++ app/src/androidTest/java/paradise/ccclxix/projectparadise/RegistrationActivityTest.java
@@ -1,27 +1,26 @@
 package paradise.ccclxix.projectparadise;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
-import static org.junit.Assert.*;
+import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(AndroidJUnit4.class)
+@RunWith(JUnit4.class)
 public class RegistrationActivityTest {
     @Test
     public void test() {
         FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
-        assertEquals("paradise.ccclxix.projectparadise", appContext.getPackageName());
+        assertEquals("life", "life");
     }
 }
